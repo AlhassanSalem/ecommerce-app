96c6548fe62b5c03c1656de56aadc7024f6999b2
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-11 13:22:13 +0200

    update login function

--- lib/database/controller/user_db_controller.dart
+++ lib/database/controller/user_db_controller.dart
@@ -1,12 +1,14 @@
 import 'package:ecommerce_app/database/DbController.dart';
+import 'package:ecommerce_app/models/process_response.dart';
 import 'package:ecommerce_app/models/user.dart';
+import 'package:ecommerce_app/sharedPreferences/shared_pref.dart';
 
 class UserDbController {
   //LOGIN , REGISTER
 
   final database = DbController().database;
 
-  void login(String email, String password) async{
+  Future<ProcessResponse> login(String email, String password) async{
     List<Map<String, dynamic>> rowsMap = await database.query(
       User.tableName,
       where: 'email = ? AND password = ?',
@@ -15,8 +17,10 @@ class UserDbController {
 
     if(rowsMap.isNotEmpty) {
       User user = User.fromMap(rowsMap.first);
-      //Shared Preference Code...
+      SharedPrefController().save(user);
+      return const ProcessResponse(message: 'Logged in Successfully', success: true);
     }
+    return const ProcessResponse(message: 'Credential error, check and try again!',success: false);
   }
 
   void register(User user) async{
