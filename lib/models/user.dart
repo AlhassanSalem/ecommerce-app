e41998eaec7bd0cc5db5eb32c15ec12cd1bf034b
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-11 11:10:15 +0200

    create user class

---
+++ lib/models/user.dart
@@ -0,0 +1,27 @@
+class User {
+  late int id;
+  late String name;
+  late String email;
+  late String password;
+
+  User();
+
+  /// READ ROW DATA FROM DATABASE TABLE
+  User.fromMap(Map<String,dynamic> rowMap) {
+    id = rowMap['id'];
+    name = rowMap['name'];
+    email = rowMap['email'];
+    password = rowMap['password'];
+  }
+
+  /// PREPARE MAP TO BE SAVED IN DATABASE
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = <String, dynamic>{};
+    map['name'] = name;
+    map['email'] = email;
+    map['password'] = password;
+    map['name'] = name;
+
+    return map;
+  }
+}
