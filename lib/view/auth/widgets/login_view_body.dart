d82e62e896527da3275ed092274f52c6a136d7a5
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-12 10:07:50 +0200

    update loginViewBody

---
+++ lib/view/auth/widgets/login_view_body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginViewBody extends StatefulWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  State<LoginViewBody> createState() => _LoginViewBodyState();
+}
+
+class _LoginViewBodyState extends State<LoginViewBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        Text('Welcome Back...'),
+        Text('Please Enter email and password...'),
+      ),
+    );
+  }
+}
