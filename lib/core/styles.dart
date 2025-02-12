c2cbde12ebce0e61dfa51f4e968d323472daa17e
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-12 10:27:44 +0200

    create class styles

---
+++ lib/core/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+abstract class Styles {
+  static TextStyle textStyle18 = GoogleFonts.montserrat(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+}
