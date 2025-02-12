169ffcb621b4d53b3e5c66a89a35f39c8c9d6d65
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-12 14:58:13 +0200

    update showSnakBar : text, behavior, showCloseIcon

--- lib/core/extension/extension_context.dart
+++ lib/core/extension/extension_context.dart
@@ -1,12 +1,20 @@
 import 'package:ecommerce_app/core/constants.dart';
+import 'package:ecommerce_app/core/styles.dart';
 import 'package:flutter/material.dart';
 
 extension ExtensionContext on BuildContext {
   showSnakBar({required String message, bool success = false}) {
     ScaffoldMessenger.of(this).showSnackBar(
       SnackBar(
-        content: Text(message),
+        content: Text(
+          message,
+          style: Styles.textStyle14.copyWith(color: Colors.white),
+        ),
         backgroundColor: success ? kPrimaryColor : Colors.red,
+        behavior: SnackBarBehavior.floating,
+        dismissDirection: DismissDirection.down,
+        closeIconColor: Colors.white,
+        showCloseIcon: true,
       ),
     );
   }
