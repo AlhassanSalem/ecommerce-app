af9e6f8cb85b7ee07a5341568cdab100561c7e28
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-12 10:58:42 +0200

    add textStyle20

--- lib/core/styles.dart
+++ lib/core/styles.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 abstract class Styles {
-  static TextStyle textStyle18 = GoogleFonts.montserrat(
-    fontSize: 18,
+
+  static TextStyle textStyle20 = GoogleFonts.montserrat(
+    fontSize: 20.sp,
     fontWeight: FontWeight.bold,
     color: Colors.black,
   );
+
+
+  static TextStyle textStyle18 = GoogleFonts.montserrat(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static TextStyle textStyle14 = GoogleFonts.montserrat(
+      fontSize: 12.sp,
+      fontWeight: FontWeight.normal,
+      color: Colors.black38,
+      height: 0.7.h);
 }
