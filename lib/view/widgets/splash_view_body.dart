24742f912ee5b794aa4d26db12625350f29bef7e
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-12 09:15:07 +0200

    update splash view body

--- lib/view/widgets/splash_view_body.dart
+++ lib/view/widgets/splash_view_body.dart
@@ -10,6 +10,10 @@ class SplashViewBody extends StatefulWidget {
 class _SplashViewBodyState extends State<SplashViewBody> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(
+        child: Text('Ecommerce app', ),
+      ),
+    );
   }
 }
