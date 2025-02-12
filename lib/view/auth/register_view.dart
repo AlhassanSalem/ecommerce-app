88c03bb4efdcca079ce84493cc02f33f0a050b85
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-12 13:57:24 +0200

    create register view

---
+++ lib/view/auth/register_view.dart
@@ -0,0 +1,12 @@
+
+import 'package:ecommerce_app/view/auth/widgets/register_view_body.dart';
+import 'package:flutter/material.dart';
+
+class RegisterView extends StatelessWidget {
+  const RegisterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const RegisterViewBody();
+  }
+}
