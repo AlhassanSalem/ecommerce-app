134b953b52a942f05f2d8c0d6e1f63309b43ce3c
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-13 09:50:43 +0200

    create logout dialog

--- lib/view/app/widgets/home_view_body.dart
+++ lib/view/app/widgets/home_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_app/core/constants.dart';
 import 'package:ecommerce_app/core/styles.dart';
+import 'package:ecommerce_app/sharedPreferences/shared_pref.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,9 +10,15 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Home',
         ),
+        actions: [
+          IconButton(
+            onPressed: () => _logout(context),
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: 10,
@@ -29,5 +37,55 @@ class HomeViewBody extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _logout(BuildContext context) async {
+    if (await _showLogoutDialog(context)) {
+      SharedPrefController().clear();
+      navigateToLogin(context);
+    }
+  }
+
+  Future<bool> _showLogoutDialog(BuildContext context) async {
+    return await showAdaptiveDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog.adaptive(
+          title: Text(
+            'Confirem Logout',
+            style: Styles.textStyle16.copyWith(color: kPrimaryColor),
+          ),
+          content: Text(
+            'Are you sure to logout?',
+            style: Styles.textStyle14,
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: Text('No',
+                  style: Styles.textStyle14.copyWith(color: kPrimaryColor)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: Text(
+                'Confirm',
+                style: Styles.textStyle14
+                    .copyWith(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void navigateToLogin(BuildContext context) {
+    Future.delayed(const Duration(milliseconds: 250), () {
+      Navigator.pushReplacementNamed(context, '/login_view');
+    });
   }
 }
