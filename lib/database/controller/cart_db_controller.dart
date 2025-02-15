43e85bc524a50e3cd45dba59bea63be5921384f4
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-15 15:04:51 +0200

    update: delete function

--- lib/database/controller/cart_db_controller.dart
+++ lib/database/controller/cart_db_controller.dart
@@ -1,16 +1,17 @@
 import 'package:ecommerce_app/database/db_oprations.dart';
 import 'package:ecommerce_app/models/cart.dart';
+import 'package:ecommerce_app/sharedPreferences/shared_pref.dart';
 
 class CartDbController extends DbOperation<Cart>{
+  int userId = SharedPrefController().getValueFor<int>(key: PrefKeys.id)!;
   @override
   Future<int> create(Cart model) async{
     return await database.insert(Cart.tableName, model.toMap());
   }
 
   @override
-  Future<bool> delete(int id) {
-    // TODO: implement delete
-    throw UnimplementedError();
+  Future<bool> delete(int id) async{
+    return await database.delete(Cart.tableName, where: 'id = ? AND user_id = ?' , whereArgs: [id, userId]) == 1;
   }
 
   @override
