0f2d9f2516da47b5a28d5b7a54c4431c10d70c1d
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-16 10:40:01 +0200

    update : changeQuantity function

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -1,46 +1,63 @@
 import 'package:ecommerce_app/database/controller/cart_db_controller.dart';
 import 'package:ecommerce_app/models/cart.dart';
+import 'package:ecommerce_app/models/process_response.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:path/path.dart';
 
-class CartProvider extends ChangeNotifier{
+class CartProvider extends ChangeNotifier {
   List<Cart> _cartItems = <Cart>[];
   final CartDbController _controller = CartDbController();
 
   List<Cart> get cartItem => _cartItems;
 
-  void create(Cart model) async{
-    bool isNotExist = _cartItems.indexWhere((cartItem) => cartItem.id == model.id) == -1;
+  void create(Cart model) async {
+    bool isNotExist =
+        _cartItems.indexWhere((cartItem) => cartItem.id == model.id) == -1;
 
-    if(isNotExist){
+    if (isNotExist) {
       int newRowId = await _controller.create(model);
       model.id = newRowId;
       _cartItems.add(model);
-    }else {
+    } else {
       int index = _cartItems.indexWhere((cartItem) => cartItem.id == model.id);
-      changeQuantity(index,model.count);
+      changeQuantity(index, model.count);
     }
   }
 
-  void changeQuantity(int index, count) {
-    if(count > 1){
-      // count
+  void changeQuantity(int index, count) async{
+    if (count >= 1) {
+      Cart model = _cartItems[index];
+      model.count = count;
+      model.total = model.count * model.price;
+      bool isUpdated = await _controller.update(model) > 1;
+      if(isUpdated){
+        _cartItems[index] = model;
+        notifyListeners();
+      }
+    } else if (count == 0) {
+      delete(_cartItems[index].id);
     }
   }
 
-  void read() async{
+  void read() async {
     _cartItems = await _controller.read();
     notifyListeners();
   }
 
-  void delete(id) async{
+  Future<ProcessResponse> delete(id) async {
     bool isDeleted = await _controller.delete(id);
-    if(isDeleted){
+    if (isDeleted) {
       int index = _cartItems.indexWhere((cartItem) => cartItem.id == id);
       _cartItems.removeAt(index);
       notifyListeners();
     }
+    return getResponse(isDeleted);
   }
 
-
-
+  ProcessResponse getResponse(bool successCondition) {
+    return ProcessResponse(
+      message: successCondition ? 'Operation Successfully' : 'Operation Failed',
+      success: successCondition,
+    );
+  }
 }
