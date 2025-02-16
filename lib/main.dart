545268649f2e39663786164af60d01f5361d6fbb
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-16 11:36:28 +0200

    add : cartProvider in MulitProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce_app/core/constants.dart';
 import 'package:ecommerce_app/core/styles.dart';
 import 'package:ecommerce_app/database/DbController.dart';
+import 'package:ecommerce_app/provider/cart_provider.dart';
 import 'package:ecommerce_app/provider/product_provider.dart';
 import 'package:ecommerce_app/sharedPreferences/shared_pref.dart';
 import 'package:ecommerce_app/view/app/cart_view.dart';
@@ -41,7 +42,10 @@ class MyApp extends StatelessWidget {
           providers: [
             ChangeNotifierProvider(
               create: (context) => ProductProvider(),
-            )
+            ),
+            ChangeNotifierProvider(
+              create: (context) => CartProvider(),
+            ),
           ],
           child: MaterialApp(
             theme: ThemeData(
@@ -98,11 +102,12 @@ class MyApp extends StatelessWidget {
               '/register_view_body': (context) => const RegisterViewBody(),
               '/home_view': (context) => const HomeView(),
               '/home_view_body': (context) => const HomeViewBody(),
-              '/operations_on_products_view': (context) => const OperationsOnProductsView(),
-              '/operations_on_products_view_body': (context) => const OperationsOnProductsViewBody(),
+              '/operations_on_products_view': (context) =>
+                  const OperationsOnProductsView(),
+              '/operations_on_products_view_body': (context) =>
+                  const OperationsOnProductsViewBody(),
               '/cart_view': (context) => const CartView(),
               '/cart_view_body': (context) => const CartViewBody(),
-
             },
           ),
         );
