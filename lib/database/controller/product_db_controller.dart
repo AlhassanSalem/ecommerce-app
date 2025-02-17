21cd35eac78ab12b0acc5c3e5dd486d792646739
alhassanalwahaidi <alhassanalwahaidi@hotmail.com>
2025-02-17 13:46:11 +0200

    update : update function

--- lib/database/controller/product_db_controller.dart
+++ lib/database/controller/product_db_controller.dart
@@ -1,9 +1,11 @@
+import 'package:ecommerce_app/database/DbController.dart';
 import 'package:ecommerce_app/database/db_oprations.dart';
 import 'package:ecommerce_app/models/product.dart';
 import 'package:ecommerce_app/sharedPreferences/shared_pref.dart';
+import 'package:sqflite/sqflite.dart';
 
 class ProductDbController extends DbOperation<Product> {
-  int userId = SharedPrefController().getValueFor(key: PrefKeys.id);
+  int userId = SharedPrefController().getValueFor<int>(key: PrefKeys.id)!;
 
   @override
   Future<int> create(Product model) async {
@@ -13,24 +15,28 @@ class ProductDbController extends DbOperation<Product> {
 
   @override
   Future<bool> delete(int id) async {
-    int countOfRowsDeleted = await database.delete(
-      Product.tableName,
-      where: 'id = ? AND user_id = ?',
-      whereArgs: [id,userId]
-    );
+    int countOfRowsDeleted = await database.delete(Product.tableName,
+        where: 'id = ? AND user_id = ?', whereArgs: [id, userId]);
 
     return countOfRowsDeleted > 0;
   }
 
   @override
-  Future<List<Product>> read() async{
-    List<Map<String,dynamic>> rowsMap = await database.query(Product.tableName);
-    return rowsMap.map((rowMap) => Product.fromMap(rowMap)).toList();;
+  Future<List<Product>> read() async {
+    List<Map<String, dynamic>> rowsMap =
+        await database.query(Product.tableName);
+    return rowsMap.map((rowMap) => Product.fromMap(rowMap)).toList();
   }
 
   @override
-  Future<int> update(Product model) async{
-    int countOfRowsUpdated = await database.update(Product.tableName, model.toMap());
-    return countOfRowsUpdated;
+  Future<int> update(Product model) async {
+    //print('From product controller : ${model.id},${model.userId},${model.price},${model.name},${model.quantity},${model.info},');
+    return await database.update(
+      Product.tableName,
+      model.toMap(),
+      where: 'id = ? AND user_id = ?',
+      whereArgs: [model.id, userId]
+    );
+
   }
 }
